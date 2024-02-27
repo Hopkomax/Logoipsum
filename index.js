00909b53dee5cc502d53e49b0f5292591298ce84
Hopkomax <gopkomax@gmail.com>
2024-02-27 20:26:23 +0200

    ready layout (first version)

---
+++ index.js
@@ -0,0 +1,5 @@
+// document.getElementById("toggle").addEventListener("change", function () {
+//   document.querySelectorAll(".toggle-text").forEach(function (el) {
+//     el.classList.toggle("active");
+//   });
+// });
